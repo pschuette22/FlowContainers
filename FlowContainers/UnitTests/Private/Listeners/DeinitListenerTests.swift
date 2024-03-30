974fa8547157c5cfc785bd6519a1dd58acdf0397
Pete <pschuette22@gmail.com>
2024-03-30 17:16:24 -0500

    deinit listeners

---
+++ FlowContainers/UnitTests/Private/Listeners/DeinitListenerTests.swift
@@ -0,0 +1,25 @@
+//
+//  DeinitListenerTests.swift
+//  FlowContainers
+//
+//  Created by Peter Schuette on 3/30/24.
+//
+
+import Foundation
+@testable import FlowContainers
+import XCTest
+import UIKit
+
+public final class DeinitListenerTests: XCTestCase {
+    @MainActor
+    func test_whenObservedObjectDeinits_onDeinitIsCalled() async {
+        let expectation = XCTestExpectation(description: "DeinitCallback is called")
+        autoreleasepool {
+            let viewController = UIViewController()
+            let listener = DeinitListener(observe: viewController) {
+                expectation.fulfill()
+            }
+        }
+        await fulfillment(of: [expectation], timeout: .normalTimeout)
+    }
+}
