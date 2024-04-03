46bdba621105ed6993c3ccf36e4ef1fefb5774d6
Pete <pschuette22@gmail.com>
2024-04-03 00:29:44 -0500

    recreated project file for testing

---
+++ FlowContainers/FlowContainers/FlowContainers.h
@@ -0,0 +1,18 @@
+//
+//  FlowContainers.h
+//  FlowContainers
+//
+//  Created by Peter Schuette on 4/3/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FlowContainers.
+FOUNDATION_EXPORT double FlowContainersVersionNumber;
+
+//! Project version string for FlowContainers.
+FOUNDATION_EXPORT const unsigned char FlowContainersVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FlowContainers/PublicHeader.h>
+
+
