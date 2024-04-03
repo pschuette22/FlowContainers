ec0f9f9ec704cd79e8b36e8a3abf7ddcc1381e48
Pete <pschuette22@gmail.com>
2024-04-02 22:31:57 -0500

    docc documentation

--- FlowContainers/Package/Source/Public/FlowContainer/FlowContainer.swift
+++ FlowContainers/Package/Source/Public/FlowContainer/FlowContainer.swift
@@ -8,7 +8,7 @@
 import Foundation
 import UIKit
 
-/// Base class for a container object which bundles a collection of ``UIViewControllers``s together to produce a unified experience or composed result
+/// Base class for a container object which bundles a collection of ``UIKit/UIViewController``s together to produce a unified experience or composed result
 ///
 /// FlowContainer must be subclassed with ``FlowContainer/initialViewController()`` overridden.
 /// ```swift
@@ -53,18 +53,18 @@ open class FlowContainer: NSObject {
         self.bindingDelegate = bindingDelegate ?? (self as? NavigationBindingDelegate)
     }
 
-    /// Provide the initial ``UIViewController`` for the flow.
+    /// Provide the initial ``UIKit/UIViewController`` for the flow.
     /// Do not retain this view controller within the flow, it should only be composed.
     /// **GOTCHA** This must be implemented! Fail loudly if it is not
     open func initialViewController() -> UIViewController {
         fatalError("`initialViewController()` was not overridden in \(String(describing: type(of: self)))!")
     }
 
-    /// Start the flow using the provided ``UINavigationController``.
-    /// This attaches the FlowContainer implementation to the ``UINavigationController`` and pushes the initial ``UIViewController`` onto it's navigation stack
+    /// Start the flow using the provided ``UIKit/UINavigationController``.
+    /// This attaches the FlowContainer implementation to the ``UIKit/UINavigationController`` and pushes the initial ``UIKit/UIViewController`` onto it's navigation stack
     /// - Parameters:
-    ///   - navigationController: ``UINavigationController`` the FlowContainer should attach to
-    ///   - animated: True if initial ``UIViewController`` should be pushed onto the ``UINavigationController`` stack with default animation. Defaults to false when the NavigationStack is empty and true when it is not
+    ///   - navigationController: ``UIKit/UINavigationController`` the FlowContainer should attach to
+    ///   - animated: True if initial ``UIKit/UIViewController`` should be pushed onto the ``UIKit/UINavigationController`` stack with default animation. Defaults to false when the NavigationStack is empty and true when it is not
     ///   - completion: (optional) completion to be called after the flow is started
     public func start(
         on navigationController: UINavigationController,
@@ -81,7 +81,7 @@ open class FlowContainer: NSObject {
     }
 
     /// Unwind and, in turn, deallocate the ``FlowContainer``
-    /// This function will dismiss all presented ``UIViewController``s before unwinding its views from the navigation stack
+    /// This function will dismiss all presented ``UIKit/UIViewController``s before unwinding its views from the navigation stack
     /// - Parameters:
     ///   -  animated: True if the dismiss and unwind animations should be done with animations.
     ///   - completion: (optional) completion to be called after the flow is completed
@@ -111,21 +111,21 @@ open class FlowContainer: NSObject {
 // MARK: - Child Access
 
 public extension FlowContainer {
-    /// Retrieve a ``UIViewController`` on the navigation stack via an ID
-    /// - Parameter id: Unique Identifier for this ``UIViewController``
-    /// - Returns: ``UIViewController`` identified by _id_, if it exists, on the navigation stack. Nil if no such ViewController is found
+    /// Retrieve a ``UIKit/UIViewController`` on the navigation stack via an ID
+    /// - Parameter id: Unique Identifier for this ``UIKit/UIViewController``
+    /// - Returns: ``UIKit/UIViewController`` identified by _id_, if it exists, on the navigation stack. Nil if no such ViewController is found
     func pushedViewController(id: String) -> UIViewController? {
         pushedControllerObservers.object(forKey: NSString(string: id))
     }
 
-    /// An array of ``UIViewController``s pushed by this FlowContainer in the order they exist on the NavigationStack.
+    /// An array of ``UIKit/UIViewController``s pushed by this FlowContainer in the order they exist on the NavigationStack.
     var orderedPushedControllers: [UIViewController] {
         let pushedControllerHashes = Set(pushedControllers.map(\.hash))
         return navigationController?.viewControllers.filter { pushedControllerHashes.contains($0.hash)
         } ?? []
     }
 
-    /// An array of ``UIViewController``s pushed by this FlowContainer. Order is not guaranteed.
+    /// An array of ``UIKit/UIViewController``s pushed by this FlowContainer. Order is not guaranteed.
     var pushedControllers: [UIViewController] {
         pushedControllerObservers
             .objectEnumerator()?
@@ -139,7 +139,7 @@ public extension FlowContainer {
 public extension FlowContainer {
     /// Push a ViewController onto the navigation stack
     /// - Parameters:
-    ///   - viewController: ``UIViewController`` to be pushed
+    ///   - viewController: ``UIKit/UIViewController`` to be pushed
     ///   - id: Unique identifier for this ViewController. Generated by default.
     ///   - animated: True if the ViewController should be pushed with animation. True by default
     ///   - completion: (optional) completion to be called after the flow is pushed onto the stack
@@ -164,7 +164,7 @@ public extension FlowContainer {
 
     /// Present a ViewController modally
     /// - Parameters:
-    ///   - viewController: ``UIViewController`` to be presented modally
+    ///   - viewController: ``UIKit/UIViewController`` to be presented modally
     ///   - animated: True if the ViewController should be presented with animation. True by default
     ///   - completion: (optional) completion to be called after the ViewController is presented over the current flow
     func present(
@@ -207,7 +207,7 @@ public extension FlowContainer {
     /// Present an instance of ``FlowContainer`` modally over the current flow
     /// - Parameters:
     ///   - flowContainer: ``FlowContainer`` to be presented
-    ///   - navigationController: ``UINavigationController`` the presented FlowContainer should attach to
+    ///   - navigationController: ``UIKit/UINavigationController`` the presented FlowContainer should attach to
     ///   - animated: True if transition should be animated. True by default.
     ///   - completion: (optional) completion to be called after the new flow is presented over the current flow
     func present(
@@ -237,7 +237,7 @@ public extension FlowContainer {
 // MARK: - Pop
 
 public extension FlowContainer {
-    /// Pop navigation stack back to a ``UIViewController`` based on position in the flow
+    /// Pop navigation stack back to a ``UIKit/UIViewController`` based on position in the flow
     /// If position is out of index, nothing happens.
     /// - Parameters:
     ///   - position: Position within the flow the NavigationStack should be popped to.
@@ -252,7 +252,7 @@ public extension FlowContainer {
     /// Pop the navigation stack back to a view controllre based on the view controller's unique identifier.
     /// If a ViewController with id is not found, nothing happens.
     /// - Parameters:
-    ///   - id: Unique identifier of the ``UIViewController``
+    ///   - id: Unique identifier of the ``UIKit/UIViewController``
     ///   - animated: True if the transition should be animated. True by default
     ///   - completion: (optional) completion to be called once the ViewController specified by ID is at the top of the navigation stack
     func pop(toControllerWithId id: String, animated: Bool = true, completion: (() -> Void)? = nil) {
@@ -265,7 +265,7 @@ public extension FlowContainer {
 // MARK: - Attach / Detach
 
 extension FlowContainer {
-    /// Attach the flow coordinator a ``UINavigationController``
+    /// Attach the flow coordinator a ``UIKit/UINavigationController``
     /// - Parameter navigationController: controller that flow container should attach to
     private func attach(to navigationController: UINavigationController) {
         guard self.navigationController == nil else {
@@ -294,9 +294,9 @@ extension FlowContainer {
         }
     }
 
-    /// Explicitly detach the FlowContainer from the ``UINavigationController``
-    /// **NOTE:** This does _not_ unwind the child ``UIViewController``s presented by the FlowContainer
-    /// If you want to unwind the FlowContainer before detaching look for ``FlowContainer/finish(animated:)``
+    /// Explicitly detach the FlowContainer from the ``UIKit/UINavigationController``
+    /// **NOTE:** This does _not_ unwind the child ``UIKit/UIViewController``s presented by the FlowContainer
+    /// If you want to unwind the FlowContainer before detaching look for ``FlowContainer/finish(animated:completion:)``
     public func detach() {
         guard let navigationController else { return }
 
